8d02dc53fb9de9b512000e57f06b72dfa6ee7250
Booker Robinson <robinson.booker11@gmail.com>
2020-02-23 18:01:02 -0400

    add loadmore button to news feed

---
+++ public/js/newsfeed.js
@@ -0,0 +1,60 @@
+const create = e => document.createElement(e);
+Element.prototype.class = function(c) {this.classList.add(c); return this}
+Element.prototype.link  = function(h) {this.href = h;         return this}
+Element.prototype.txt  = function(t) {this.textContent = t;    return this}
+
+S(".loadmore-btn").addEventListener("click", function () {
+  this.classList.toggle("hidden");
+  S(".loadmore_loader").classList.toggle("hidden");
+  let offset = S(".posts").children.length
+  Ajax("Get", `/?json=true&offset=${offset}`, "")
+      .then(posts => posts.forEach(post => render(post)))
+      .then(() => {
+        this.classList.toggle("hidden");
+        S(".loadmore_loader").classList.toggle("hidden");
+      })
+})
+
+function render(post){
+  let postDiv     = create("div").class("post"),
+      img         = create("img"),
+      comments_a  = create("a").link(`/post/${post._id}/comments`),
+      uname_h2    = create("h2"),
+      uname_a2    = create("a").link(`/user/${post.owner._id}`).txt(post.owner.username),
+      h3          = create("h3").txt(new Date(post.created).toString().substring(0, 15)),
+      p           = create("p").txt(post.text.substring(0, 290)),
+      span        = create("span").class("readmore").txt("..."),
+      a3          = create("a").link(`/post/${post._id}`).txt("read more");
+      uname_h2   .append(uname_a2)
+      span       .append(a3)
+      p          .append(span)
+      postDiv    .append(img)
+      postDiv    .append(comments_a)
+      postDiv    .append(uname_h2)
+      postDiv    .append(h3)
+      postDiv    .append(p)
+      S(".posts").append(postDiv)
+      img.src = post.image
+      comments_a.innerHTML = `<aside class="comments"><strong><i class="far fa-comment"></i> ${post.comments.length}</strong></aside>`
+}
+
+function Ajax(method, url, data) {
+    var XHR = new XMLHttpRequest();
+  
+    var P = new Promise(function(resolve, reject){
+      XHR.onreadystatechange = function(){
+          if(XHR.readyState == 4){
+            if(XHR.status == 200){
+              resolve(JSON.parse(XHR.responseText));
+            }
+            else{
+              reject(Error("There was an error"));
+            }
+          }
+        }
+    });
+    XHR.open(method, url);
+    XHR.setRequestHeader("content-type", "application/json");
+    XHR.send(data);
+    return P;
+  };
--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,27 @@ var express           = require("express"),
 router.get("/", async function(req, res){
     if(!req.user) return res.render("index/landing");
     if(!req.user.following[0]) return res.render("user/feed", {posts: []});
-
-    var followingArr = req.user.following.reduce(function(acc, val) {
-      acc.push({"owner.id": val.toString()});
-      return acc;
-    }, []);
-
-    Post.find({$or: followingArr}).sort({"created": -1}).exec(function(err, recentPosts){
-        if(err || !recentPosts) return res.render("user/feed", {posts: []});
-        recentPosts = recentPosts.splice(0, 10);
-        console.log(req.query.json)
-        if(req.query.json) return res.json(recentPosts)
-        res.render("user/feed", {posts: recentPosts});
-    });
+    console.log(req.query)
+    getFeedItems(req.user, Number(req.query.offset) || 0, Number(req.query.limit) || 10, (err, posts) => {
+        console.log(posts)
+        if(err || !posts) res.render("user/feed", {posts: []});
+        else if(req.query.json) res.json(posts)
+        else res.render("user/feed", {posts: posts});
+    })
 });
+
+function getFeedItems(user, offset, limit, cb){
+  let followingArr = user.following.reduce(function(acc, val) {
+    acc.push({"owner.id": val.toString()});
+    return acc;
+  }, []);
+  console.log(`offset: ${offset} limit: ${limit}`)
+  Post.find({$or: followingArr})
+        .sort({"created": -1})
+        .skip(offset)
+        .limit(limit)
+        .exec(cb);
+}
 
 router.get("/users", function(req, res) {
         var query = req.sanitize(req.query.search);
